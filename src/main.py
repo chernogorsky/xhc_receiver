753d39615ac189215a8e5137f27d6d17415528d0
Simão Amorim <simao_amorim@outlook.pt>
2020-03-03 22:50:25 +0000

    Improve connection initialization

--- src/main.py
+++ src/main.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
 import sys
-import os
-import hid
+import struct
+import hidapi
+
+fields = ['key1', 'key2', 'sel_incr', 'sel_axis', 'mpg_incr']
+previous = dict.fromkeys(fields)
 
 if __name__ == "__main__":
-	vendor_id = 4302 # 10CE
-	product_id = 60307 # EB93
-	device_list = hid.enumerate(vendor_id, product_id)
-	device_dict = next(dev for dev in device_list if dev['usage'] == 0)
-	device = hid.device()
-	device.open_path(device_dict['path'])
+	vendor_id = 0x10CE
+	product_id = 0xEB93
+	device_info = list(hidapi.enumerate(vendor_id, product_id))
+	if not device_info:
+		print('No device found')
+		exit(1)
+	device_info = device_info[0]
+	print('Device found!')
+	print('Device parameters:')
+	for attr in device_info.__slots__:
+		print('\t', attr, ':', device_info.__getattribute__(attr))
+	device = hidapi.Device
+	try:
+		device = hidapi.Device(device_info, blocking=True)
+	except OSError as e:
+		print(e, file=sys.stderr)
+		exit(2)
 	print("Manufacturer: ", device.get_manufacturer_string())
 	print("Product: ", device.get_product_string())
-	print("Features: ", device.get_feature_report(0, 100))
 	try:
 		while 1:
 			print("Read: ", device.read(100))
