9a8202894b19a51c3753134d59fabdcebcf2ab24
Simão Amorim <simao_amorim@outlook.pt>
2020-06-06 13:40:16 +0100

    Move from hidapi library to hid library

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import struct
-import hidapi
+import hid
 import threading
 import queue
 import json
@@ -9,37 +9,30 @@ import socket
 
 fields = ['key1', 'key2', 'sel_incr', 'sel_axis', 'mpg_incr']
 queue = queue.Queue(0)
-previous = dict.fromkeys(fields)
 
 
 def receiver():
 	while True:
-		#time.sleep(0.001)
-		frame = device.read(8)
-		if frame:
+		frame = device.read(8, timeout=None)
+		if frame is not None:
 			queue.put_nowait(frame)
 
 
 if __name__ == "__main__":
 	vendor_id = 0x10CE
 	product_id = 0xEB93
-	device_info = list(hidapi.enumerate(vendor_id, product_id))
+	device_info = hid.enumerate(vendor_id, product_id)
 	if not device_info:
 		print('No device found')
 		exit(1)
-	device_info = device_info[0]
+	device_info = device_info.__getitem__(0)
 	print('Device found!')
-	print('Device parameters:')
-	for attr in device_info.__slots__:
-		print('\t', attr, ':', device_info.__getattribute__(attr))
-	device = hidapi.Device
+	device = hid.Device
 	try:
-		device = hidapi.Device(device_info, blocking=True)
+		device = hid.Device(path=device_info.__getitem__('path'))
 	except OSError as e:
 		print(e, file=sys.stderr)
 		exit(2)
-	print("Manufacturer: ", device.get_manufacturer_string())
-	print("Product: ", device.get_product_string())
 	try:
 		t1 = threading.Thread(target=receiver)
 		t1.daemon = True
