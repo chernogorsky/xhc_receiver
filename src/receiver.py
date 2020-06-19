3e0ed7317acd75e34530f219bf9f5bb3b6eca52c
Simão Amorim <simao_amorim@outlook.pt>
2020-06-19 12:20:36 +0100

    Move class to top of the file

--- src/receiver.py
+++ src/receiver.py
@@ -6,61 +6,6 @@ fields = ['key1', 'key2', 'sel_incr', 'sel_axis', 'mpg_incr']
 q = queue.Queue(100)
 
 
-def key1_action(x):
-    return {
-        0: 'noop',
-        1: 'reset',
-        2: 'stop',
-        3: 'start_pause',
-        4: 'feed_plus',
-        5: 'feed_minus',
-        6: 'spindle_plus',
-        7: 'spindle_minus',
-        8: 'machine_home',
-        9: 'safe_z',
-        10: 'work_home',
-        11: 'spindle_on_off',
-        12: 'fn',
-        13: 'probe_z',
-        14: 'continuous',
-        15: 'step',
-        16: 'noop'
-    }[x]
-
-
-def key2_action(x):
-    return {
-        0: 'noop',
-        1: 'noop',
-        2: 'noop',
-        3: 'noop',
-        4: 'macro_1',
-        5: 'macro_2',
-        6: 'macro_3',
-        7: 'macro_4',
-        8: 'macro_5',
-        9: 'macro_6',
-        10: 'macro_7',
-        11: 'macro_8',
-        12: 'noop',
-        13: 'macro_9',
-        14: 'noop',
-        15: 'noop',
-        16: 'macro_10'
-    }[x]
-
-
-def axis_selection(x):
-    return {
-        0: 'x',
-        1: 'y',
-        2: 'z',
-        3: 'a',
-        4: 'b',
-        5: 'c'
-    }[x]
-
-
 class Receiver(threading.Thread):
     def __init__(self, input_queue: queue.Queue, device):
         self.queue = input_queue
@@ -107,3 +52,58 @@ class Receiver(threading.Thread):
         self.interrupt = True
 
 
+def key1_action(x):
+    return {
+        0: 'noop',
+        1: 'reset',
+        2: 'stop',
+        3: 'start_pause',
+        4: 'feed_plus',
+        5: 'feed_minus',
+        6: 'spindle_plus',
+        7: 'spindle_minus',
+        8: 'machine_home',
+        9: 'safe_z',
+        10: 'work_home',
+        11: 'spindle_on_off',
+        12: 'fn',
+        13: 'probe_z',
+        14: 'continuous',
+        15: 'step',
+        16: 'noop'
+    }[x]
+
+
+def key2_action(x):
+    return {
+        0: 'noop',
+        1: 'noop',
+        2: 'noop',
+        3: 'noop',
+        4: 'macro_1',
+        5: 'macro_2',
+        6: 'macro_3',
+        7: 'macro_4',
+        8: 'macro_5',
+        9: 'macro_6',
+        10: 'macro_7',
+        11: 'macro_8',
+        12: 'noop',
+        13: 'macro_9',
+        14: 'noop',
+        15: 'noop',
+        16: 'macro_10'
+    }[x]
+
+
+def axis_selection(x):
+    return {
+        0: None,
+        6: None,
+        17: 'x',
+        18: 'y',
+        19: 'z',
+        20: 'a',
+        21: 'b',
+        22: 'c',
+    }[x]
